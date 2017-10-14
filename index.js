0c59ac9f617f9e67a174b25904ea2ddb2b798dad
Nathaniel Woodbury <nxtezzz@gmail.com>
2017-10-14 05:36:02 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// write your code below!
+function happyHolidays () {
+  return 'Happy holidays!'
+}
+function happyHolidayTo (name) {
+ return `Happy holidays, ${name}!`
+}
+function happyHolidayTo (holiday,name) {
+  return `Happy ${holiday}, ${name}!`
+}
